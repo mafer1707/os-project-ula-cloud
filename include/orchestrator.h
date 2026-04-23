4b45a4ec51929f3d970216e7a60bb6cd156d1618
mafer1707 <mafercasmen@gmail.com>
2026-04-23 16:03:13 -0400

    state_names deleted

--- include/orchestrator.h
+++ include/orchestrator.h
@@ -23,15 +23,6 @@ typedef enum {
     STATE_STOPPED    // Terminado normalmente (exit code 0)
 } service_state_t;
 
-//**  Esto es para imprimir los nombre de los estados y no sus números
-const char *state_names[] = {
-    "IDLE", 
-    "RUNNING", 
-    "CRASHED", 
-    "KILLED",   
-    "STOPPED"
-};
-
 //
 
 /**
